e5661ecdd94831d9fd6c2166e8a7514603724e98
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-02-24 10:06:12 +0000

    Add a comment to clarify the reasoning behind runtime downloads

--- aws/get-openshift-install.sh
+++ aws/get-openshift-install.sh
@@ -3,6 +3,10 @@
 # Copyright 2021 Authors of Cilium
 # SPDX-License-Identifier: Apache-2.0
 
+# this script downloads appropriate openshift-install binary based on given distro
+# and version; runtime downloads are prefer because it wouldn't be feasible to include
+# all possible versions in the terraform executor image
+
 set -o errexit
 set -o pipefail
 set -o nounset
