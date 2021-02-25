cac2ca099210d49694366f26c9980f2c84ef5b23
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-02-25 17:40:29 +0000

    Add ingress rules for Cilium ports (ref #1)

---
+++ aws/cilium_ingress_rules.tf
@@ -0,0 +1,44 @@
+locals {
+  cilium_ports = [
+      {
+        port = 8472
+        protocol = "udp"
+      },
+      {
+        port = 4240
+        protocol = "tcp"
+      }
+  ]
+
+  cilium_ingress_rules = flatten([
+    for pair in setproduct(local.cilium_ports, setproduct([local.worker_sg], [local.master_sg])) : [
+      {
+        port = pair[0].port
+        protocol = pair[0].protocol
+        security_group_id = pair[1][0]
+        source_security_group_id = pair[1][0]
+      },
+      {
+        port = pair[0].port
+        protocol = pair[0].protocol
+        security_group_id = pair[1][0]
+        source_security_group_id = pair[1][1]
+      },
+    ]
+  ])
+}
+
+
+resource aws_security_group_rule cilium_ingress_rules {
+  for_each = {
+    for index, rule in local.cilium_ingress_rules : "rule_${index}_${rule.port}_${rule.protocol}" => rule
+  }
+
+  type = "ingress"
+
+  from_port = each.value.port
+  to_port = each.value.port
+  protocol = each.value.protocol
+  security_group_id = each.value.security_group_id 
+  source_security_group_id = each.value.source_security_group_id 
+}
