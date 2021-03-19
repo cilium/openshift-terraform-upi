2bed50380bee85874ef93a0289835a3971a74c64
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-03-19 14:29:02 +0000

    Ensure ELB SG is deleted also 

--- aws/ensure-elb-is-deleted.sh
+++ aws/ensure-elb-is-deleted.sh
@@ -15,9 +15,14 @@ terraform output -json aws_config | jq -r > "${AWS_CONFIG_FILE}"
 aws configure list
 
 vpc_id="${1}"
-
-elb_names=($(aws elb describe-load-balancers | jq -r --arg vpc_id "${vpc_id}" '.LoadBalancerDescriptions[] | select(.VPCId == $vpc_id) | .LoadBalancerName'))
+elb_desc_json="$(aws elb describe-load-balancers)"
+elb_names=($(echo "${elb_desc_json}" | jq -r --arg vpc_id "${vpc_id}" '.LoadBalancerDescriptions[] | select(.VPCId == $vpc_id) | .LoadBalancerName'))
+elb_security_groups=($(echo "${elb_desc_json}" | jq -r --arg vpc_id "${vpc_id}" '.LoadBalancerDescriptions[] | select(.VPCId == $vpc_id) | .SecurityGroups[]'))
 
 for elb_name in "${elb_names[@]}" ; do
   aws elb delete-load-balancer --load-balancer-name "${elb_name}"
+done
+
+for elb_security_group in "${elb_security_groups[@]}" ; do
+  aws ec2 delete-security-group --group-id "${elb_security_group}"
 done
