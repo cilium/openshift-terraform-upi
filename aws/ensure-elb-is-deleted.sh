a13ba2b7ebbd76e64a0cd0f7f38b4669877953ca
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-03-19 17:55:04 +0000

    Retry SG deletion

--- aws/ensure-elb-is-deleted.sh
+++ aws/ensure-elb-is-deleted.sh
@@ -20,9 +20,18 @@ elb_names=($(echo "${elb_desc_json}" | jq -r --arg vpc_id "${vpc_id}" '.LoadBala
 elb_security_groups=($(echo "${elb_desc_json}" | jq -r --arg vpc_id "${vpc_id}" '.LoadBalancerDescriptions[] | select(.VPCId == $vpc_id) | .SecurityGroups[]'))
 
 for elb_name in "${elb_names[@]}" ; do
+  echo "will delete ELB ${elb_name}"
   aws elb delete-load-balancer --load-balancer-name "${elb_name}"
 done
 
 for elb_security_group in "${elb_security_groups[@]}" ; do
-  aws ec2 delete-security-group --group-id "${elb_security_group}"
+  echo "will delete SG ${elb_security_group}"
+  until aws ec2 delete-security-group --group-id "${elb_security_group}" ; do
+    # it's expected for ELB deletion to take some time for all resources to go
+    # it's also expected that this script succeeds on the first attempt, as on
+    # second attempt there is no ELB any more, finding out the SG is would
+    # require an additional API call, which doesn't seem necessary otherwise
+    echo "failed to delete ${elb_security_group}, will keep trying..."
+    sleep 1
+  done
 done
