129612af8589223cf53a5a226d91dcacfedb183f
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-06-09 13:32:18 +0100

    Bump OLM repo

--- openshift_install_config/modules.tf
+++ openshift_install_config/modules.tf
@@ -1,6 +1,6 @@
 module cilium_olm {
   # this needs to be kept up-to-date as new Cilium releases get added to the repo
-  source = "git::https://github.com/cilium/cilium-olm.git?ref=52649c05d4889f3a9c6f9f714cd52de659d1a30d"
+  source = "git::https://github.com/cilium/cilium-olm.git?ref=ae233fd9433f29a14c165435fcedcf16a635cd3e"
 }
 
 locals {
