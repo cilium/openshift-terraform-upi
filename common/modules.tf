8adf597f23992514b0714a77bf05d3cfe9724f5b
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-07-12 14:47:36 +0100

    Bump OLM repo

--- common/modules.tf
+++ common/modules.tf
@@ -1,6 +1,6 @@
 module cilium_olm {
   # this needs to be kept up-to-date as new Cilium releases get added to the repo
-  source = "git::https://github.com/cilium/cilium-olm.git?ref=5d31d493f7d0d60454dca19ef0f938f4631a86ff"
+  source = "git::https://github.com/cilium/cilium-olm.git?ref=e40f2209ea0e080ec89c249dedfaad88867bc2b0"
 }
 
 locals {
