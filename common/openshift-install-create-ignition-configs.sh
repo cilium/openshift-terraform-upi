21ef120e4a5fe8c6426b35110728e298528f822f
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-07-13 12:00:45 +0100

    Make script output even more informative

--- common/openshift-install-create-ignition-configs.sh
+++ common/openshift-install-create-ignition-configs.sh
@@ -21,6 +21,10 @@ binary="${script_dir}/bin/openshift-install-${distro}-${version}"
 if [ "$#" -gt 0 ] ; then
   extra_manifests=("${@}")
   for i in "${extra_manifests[@]}" ; do cp "${i}" "${config_dir}/manifests" ; done
+  echo "wrote ${#extra_manifests[@]} extra_manifests manifests to ${config_dir}/manifests: ${extra_manifests[@]}"
 fi
+
+
+echo "all files in ${config_dir}/manifests: $(ls "${config_dir}/manifests")"
 
 "${binary}" create ignition-configs --dir "${config_dir}"
